e2dd7ffc84affa55e57dfe2ea6f9624a404623d2
u4434246 <u4434246@lgn301.nchc-2020-hpc>
2023-05-18 22:58:22 +0800

    feat/get_caption

--- back-end/server.py
+++ back-end/server.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import json
+import subprocess
 
 app = Flask(__name__, template_folder='../front-end', static_folder='../front-end/static')
 
@@ -6,6 +8,34 @@ app = Flask(__name__, template_folder='../front-end', static_folder='../front-en
 def index():
     return render_template("index.html")
 
+@app.route('/caption/<video_id>', methods=['GET'])
+def getCaption(video_id):
+    data_file = "anet_data.json"
+    output_file = "data.json"
+
+    with open(data_file) as f:
+        data = json.load(f)
+    if video_id in data:
+        video_data = {video_id: data[video_id]}
+        with open(output_file, 'w') as f:
+            json.dump(video_data, f, indent=4)
+    else:
+        print("Video ID not found in the JSON data.")
+    
+    p = subprocess.run(['./translate.sh'])
+    if p.returncode == 0:
+        with open('results/results.json') as f:
+            data = json.load(f)
+
+        if "results" in data:
+                results = data["results"]
+                extracted_data = results[video_id]
+                response = json.dumps(extracted_data, indent=4)
+                return response
+        else:
+            return []
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # response = getCaptioning("v_HWV_ccmZVPA")
+    # print(response)
---
+++ front-end/static/js/scripts.js
@@ -0,0 +1,9 @@
+
+async function getCaption() {
+    console.log('getCaption() called');
+    video_id = 'v_lVu-4SKcb4c'
+    const response = await fetch('/caption/' + video_id);
+    const data = await response.json();
+    console.log(data);
+    // document.getElementById('caption').innerHTML = data.caption;
+}
